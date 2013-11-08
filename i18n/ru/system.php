4b07bb8b16ed1c2a609eeab40a6c32903fcf509f
Vladislav Bakin <vladislav.bakin@umisoft.ru>
2013-11-08 14:23:33 +0400

    Remove i18n folder

--- i18n/ru/system.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Словарь для локализации системных сообщений фреймворка
- */
-return array(
-    'Component "{name}" is not registered in component "{path}".' => 'Компонент "{name}" не зарегистрирован в "{path}".',
-    'Component instance "{class}" is not inited.'                 => 'Экземпляр компонента "{class}" не инициализирован.',
-);
