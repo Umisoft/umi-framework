6d2fd660166bf3e4bf586d856206cc49ac7b9697
Daria Prusova <guzhova@umisoft.ru>
2013-11-25 15:41:35 +0400

    [pagination] add TPaginationSupport for tests

---
+++ tests/utest/pagination/TPaginationSupport.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * UMI.Framework (http://umi-framework.ru/)
+ *
+ * @link      http://github.com/Umisoft/framework for the canonical source repository
+ * @copyright Copyright (c) 2007-2013 Umisoft ltd. (http://umisoft.ru/)
+ * @license   http://umi-framework.ru/license/bsd-3 BSD-3 License
+ */
+namespace utest\pagination;
+
+use umi\toolkit\IToolkit;
+
+/**
+ * Трейт для регистрации тулбокса пагинации
+ */
+trait TPaginationSupport
+{
+    /**
+     * Получить тестовый тулкит
+     * @throws \RuntimeException
+     * @return IToolkit
+     */
+    abstract protected function getTestToolkit();
+
+    protected function registerPaginationTools()
+    {
+        $this->getTestToolkit()->registerToolbox(
+            require(LIBRARY_PATH . '/pagination/toolbox/config.php')
+        );
+    }
+}
+ 
