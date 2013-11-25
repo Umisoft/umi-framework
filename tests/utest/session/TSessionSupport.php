36aefdd1a521c5e291f7f58657ca93ce89edfec2
Daria Prusova <guzhova@umisoft.ru>
2013-11-25 15:12:28 +0400

    [session] add TSessionSupport for tests

---
+++ tests/utest/session/TSessionSupport.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * UMI.Framework (http://umi-framework.ru/)
+ *
+ * @link      http://github.com/Umisoft/framework for the canonical source repository
+ * @copyright Copyright (c) 2007-2013 Umisoft ltd. (http://umisoft.ru/)
+ * @license   http://umi-framework.ru/license/bsd-3 BSD-3 License
+ */
+namespace utest\session;
+
+use umi\toolkit\IToolkit;
+
+/**
+ * Трейт для регистрации тулбокса сессий
+ */
+trait TSessionSupport
+{
+    /**
+     * Получить тестовый тулкит
+     * @throws \RuntimeException
+     * @return IToolkit
+     */
+    abstract protected function getTestToolkit();
+
+    protected function registerSessionTools()
+    {
+        $this->getTestToolkit()->registerToolbox(
+            require(LIBRARY_PATH . '/session/toolbox/config.php')
+        );
+    }
+}
+ 
